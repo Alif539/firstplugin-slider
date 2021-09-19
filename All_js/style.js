0ba58cd8f2bdb6bf46c85068cbd671a96e3f5299
Alif539 <ashfaqali36069@gmail.com>
2021-09-19 21:29:15 +0600

    understinding the concept of js plugins

---
+++ All_js/style.js
@@ -0,0 +1,21 @@
+// js for horaizontal slider
+$(document).ready(function(){
+    $('.slickOne').slick({
+        autoplay:true,
+        arrows:false,
+        adaptiveHeight:true,
+    });
+
+
+  });
+
+//   js foe vartical slider
+$(document).ready(function(){
+    $('.slickTwo').slick({
+        autoplay:true,
+        vertical:true,
+        adaptiveHeight:true,
+    });
+
+
+  });
